4ef8a070e735d6fb6e7d1f414455a53520f6a928
Luiz Eduardo <duduamaralspereira@gmail.com>
2024-09-09 10:52:10 -0300

    Inserindo dados no banco de dados

--- costs/src/components/pages/NewProject.js
+++ costs/src/components/pages/NewProject.js
@@ -1,12 +1,39 @@
+import { useNavigate } from "react-router-dom";
+
 import styles from "./../NewProject.module.css"
 import ProjectForm from "../projects/ProjectForm";
 
 function Projects() {
+  const navigate = useNavigate();;
+
+  function createPost(project){
+
+    // initialize cost and service
+
+    project.cost = 0;
+    project.services = [];
+
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        "Content-type" : "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+    .then((resp) => resp.json())
+    .then((data) =>{
+      console.log(data)
+      // redirect
+      navigate("/projects", {message: "Projeto copiado com sucesso"})
+    })
+    .catch(err => console.log(err))
+  }
+
   return (
     <div className={styles.newproject_container}>
       <h1>Criar Projetos</h1>
       <p>Crie seu projeto para depois adcionar os projetos.</p>
-      <ProjectForm btnText="Criar Projeto" />
+      <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
     </div>
   );
 }
